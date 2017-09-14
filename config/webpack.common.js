0f8990b09a36f924c246e1e884632af6e4ce89da
x7c1 <jmtaro+x7c1@gmail.com>
2017-09-14 09:35:33 +0900

    remove unused codes

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,7 +1,6 @@
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-const webpack = require('webpack');
 
 module.exports = {
   entry: {
