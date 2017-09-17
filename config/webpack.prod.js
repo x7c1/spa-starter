87168be59dcaca3c29c186e7c56ae28ad6ac9979
x7c1 <jmtaro+x7c1@gmail.com>
2017-09-17 12:41:26 +0900

    clean the /dist folder before each build

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,10 +1,17 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
   plugins: [
+    new CleanWebpackPlugin([
+      path.resolve(__dirname, '..', 'dist'),
+    ], {
+      root: path.resolve(__dirname, '..'),
+    }),
     new ExtractTextPlugin({
       filename: 'styles/[name].[contenthash].css',
       allChunks: true,
