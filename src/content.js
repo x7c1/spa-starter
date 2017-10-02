695fcb1f430a2197de3295892ca4025c82f9f160
x7c1 <jmtaro+x7c1@gmail.com>
2017-10-03 01:08:33 +0900

    add button to run lazy loading

--- src/content.js
+++ src/content.js
@@ -17,13 +17,25 @@ const createElement = () => {
   return element;
 };
 
+const createButton = () => {
+  const button = document.createElement('button');
+  button.innerHTML = 'load scripts';
+  button.onclick = event => {
+    debug(event);
+  };
+  return button;
+};
+
 export const render = () => {
   debug('[init] render');
-  const element = createElement();
+
+  const lazy = document.getElementById('sample-lazy-load');
+  lazy.appendChild(createButton());
 
   const area = document.getElementsByClassName('js-area')[0];
   area.appendChild(createImage());
 
+  const element = createElement();
   document.body.insertBefore(element, document.body.firstChild);
   debug('[done] render');
 };
@@ -31,6 +43,12 @@ export const render = () => {
 export const clean = () => {
   debug('[init] clean');
   document.body.removeChild(document.body.firstChild);
+
   const area = document.getElementsByClassName('js-area')[0];
   area.removeChild(area.firstChild);
+
+  const lazy = document.getElementById('sample-lazy-load');
+  while (lazy.firstChild) {
+    lazy.removeChild(lazy.firstChild);
+  }
 };
