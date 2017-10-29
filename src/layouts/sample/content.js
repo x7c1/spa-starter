1fe5c3ba1309963f7d2d4135cc91cddd61966f26
x7c1 <jmtaro+x7c1@gmail.com>
2017-10-30 00:04:10 +0900

    remove needless codes

--- src/layouts/sample/content.js
+++ src/layouts/sample/content.js
@@ -1,42 +1,30 @@
-import _ from 'lodash';
 import './index.scss';
 import * as imageArea from './image-area';
 import * as lazyArea from './lazy-area';
 
 const debug = require('debug')('spa-starter:sample');
 
-const createElement = () => {
-  const element = document.createElement('div');
-  element.innerHTML = _.join(['hello', 'webpack'], ', ');
-  return element;
+const content = {
+  get node() {
+    return document.getElementById('sample-content');
+  }
 };
 
 export const render = () => {
-  debug('[init] render');
+  debug('-> render');
 
-  const contentArea = document.getElementById('sample-content');
-  contentArea.innerHTML = [
+  const contents = [
     imageArea.getHtml(),
     lazyArea.getHtml(),
-  ].join('\n');
-
-  const element = createElement();
-  document.body.insertBefore(element, document.body.firstChild);
+  ];
+  content.node.innerHTML = contents.join('\n');
 
   imageArea.render();
   lazyArea.render();
+
   debug('<- render');
 };
 
 export const clean = () => {
-  debug('[init] clean');
-  document.body.removeChild(document.body.firstChild);
-
-  const area = document.getElementsByClassName('js-area')[0];
-  area.removeChild(area.firstChild);
-
-  const lazy = document.getElementById('sample-lazy-load');
-  while (lazy.firstChild) {
-    lazy.removeChild(lazy.firstChild);
-  }
+  debug('-> clean');
 };
