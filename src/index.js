ba8486ba6d537e54b072df18a9a1df09c620345d
x7c1 <jmtaro+x7c1@gmail.com>
2017-10-30 00:07:54 +0900

    rename

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import * as content from "./layouts/sample/content";
+import * as content from "./layouts/sample";
 import './index.css';
 
 content.render();
 
 if (module.hot) {
-  module.hot.accept('./layouts/sample/content', () => {
+  module.hot.accept('./layouts/sample/index', () => {
     content.clean();
     content.render();
   });
