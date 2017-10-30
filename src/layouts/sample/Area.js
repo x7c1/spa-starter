e58e7ac16722c339bf2dc87a2069a576eeaf11aa
x7c1 <jmtaro+x7c1@gmail.com>
2017-10-31 01:00:48 +0900

    add Area.js

---
+++ src/layouts/sample/Area.js
@@ -0,0 +1,45 @@
+const debug = require('debug')('spa-starter:Area');
+
+export class Area {
+
+  /**
+   * @param nodes
+   * @param {string} html
+   * @param {Function} render - void => void
+   */
+  static from(nodes, { html, render }) {
+    return new Area({
+      onHtml: f => {
+        f(html);
+        render();
+      },
+      clean: () => {
+        const { label, content } = nodes;
+        while (content.firstChild) {
+          content.removeChild(content.firstChild);
+        }
+        debug(`<- clean : ${label}`);
+      },
+    });
+  }
+
+  /**
+   * @param {Function} onHtml - (string => void) => void
+   * @param {Function} clean - void => void
+   */
+  constructor({ onHtml, clean }) {
+    this.onHtml = onHtml;
+    this.clean = clean;
+  }
+
+  append(area) {
+    const onHtml = f => this.onHtml(html1 =>
+      area.onHtml(html2 => f(html1 + html2))
+    );
+    const clean = () => {
+      this.clean();
+      area.clean();
+    };
+    return new Area({ onHtml, clean });
+  }
+}
