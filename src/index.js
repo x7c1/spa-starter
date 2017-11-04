de2f83be203152b6fd7ffe07a88eff91f0440ee3
x7c1 <jmtaro+x7c1@gmail.com>
2017-11-04 19:19:30 +0900

    rename

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import * as sample from "./layouts/sample";
+import * as sample from './layouts/samples';
 import './index.scss';
 
 sample.render();
 
 if (module.hot) {
-  module.hot.accept('./layouts/sample', () => {
+  module.hot.accept('./layouts/samples', () => {
     sample.clean();
     sample.render();
   });
