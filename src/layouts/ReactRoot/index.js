08278e5f3a7e69398d4de4b659fb72f9dddd46ff
x7c1 <jmtaro+x7c1@gmail.com>
2017-11-20 01:11:54 +0900

    extract Home component

--- src/layouts/ReactRoot/index.js
+++ src/layouts/ReactRoot/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import { Router, Route, Switch } from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory';
+import { Home } from '../pages/Home';
 import './ReactRoot.scss';
+
+const history = createBrowserHistory();
 
 const debug = require('debug')('spa-starter:ReactRoot');
 
 export class ReactRoot extends React.Component {
+
+  shouldComponentUpdate() {
+    return false;
+  }
+
   render() {
     debug('-> render');
     return (
-      <div className='react-sample-area'>
-        <h3>Hello, React!</h3>
-        <span>sample text</span>
-      </div>
+      <Router history={history}>
+        <Switch>
+          <Route path='/' exact component={Home} />
+        </Switch>
+      </Router>
     );
   }
 }
---
+++ src/layouts/pages/Home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Home = props => {
+  return (
+    <div className='react-sample-area'>
+      <h3>Hello, React!</h3>
+      <span>sample text</span>
+    </div>
+  );
+};
