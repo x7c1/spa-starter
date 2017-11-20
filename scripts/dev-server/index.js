02549e1fbd8be85f9988faeb3a799a6873c9ec44
x7c1 <jmtaro+x7c1@gmail.com>
2017-11-20 23:51:23 +0900

    enable dev-server to accept various paths

--- scripts/dev-server/index.js
+++ scripts/dev-server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const debug = require('debug')('spa-starter:dev');
 const webpack = require('webpack');
+const path = require('path');
 const config = require('../../config/webpack.dev');
 const dev = require('../../config/project.dev');
 
@@ -17,6 +18,20 @@ app.use(require('webpack-dev-middleware')(compiler, {
 app.use(require('webpack-hot-middleware')(compiler, {
   path: dev.server.hmr.path,
 }));
+
+// https://github.com/jantimon/html-webpack-plugin/issues/145#issuecomment-170554832
+app.use('*', (request, response, next) => {
+  const filename = path.join(compiler.outputPath, 'index.html');
+  compiler.outputFileSystem.readFile(filename, (err, result) => {
+    if (err) {
+      next(err);
+    } else {
+      response.set('content-type', 'text/html');
+      response.send(result);
+      response.end();
+    }
+  });
+});
 
 app.listen(dev.server.port, () => {
   debug(`Server is now running at ${dev.server.url}.`);
