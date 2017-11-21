267dd539ea2f944c1b9eb7dfc1fa16fee291063c
x7c1 <jmtaro+x7c1@gmail.com>
2017-11-21 09:18:59 +0900

    add CoreLayout

---
+++ src/layouts/CoreLayout/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const CoreLayout = props => {
+  return (
+    <div className='react-sample-area'>
+      {props.children}
+    </div>
+  );
+};
--- src/layouts/pages/Home/route.js
+++ src/layouts/pages/Home/route.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { Route } from 'react-router';
 import { Home } from './index';
+import { CoreLayout } from '../../CoreLayout/index';
 
 export default () => (
-  <Route path='/' exact component={Home} />
+  <Route
+    path='/'
+    exact
+    component={props => (
+      <CoreLayout>
+        <Home />
+      </CoreLayout>
+    )}
+  />
 );
