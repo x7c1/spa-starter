6edc9c6b9cee21f295e07575a86b170e866d8228
x7c1 <jmtaro+x7c1@gmail.com>
2017-11-21 09:40:35 +0900

    add sample menu

--- src/layouts/CoreLayout/index.js
+++ src/layouts/CoreLayout/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 export const CoreLayout = props => {
   return (
     <div className='react-sample-area'>
       <h3>Hello, React!</h3>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/sample-page">Sample Page</Link></li>
+        </ul>
+      </nav>
       {props.children}
     </div>
   );
