897c26a4de00b0a5c84f6e5308ade2f13077780b
x7c1 <jmtaro+x7c1@gmail.com>
2017-12-11 01:38:52 +0900

    apply redux-seal middleware

--- src/setup/sharedStore.js
+++ src/setup/sharedStore.js
@@ -1,9 +1,16 @@
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
+import { seal } from 'general/redux-seal';
 import { RichStore } from 'general/RichStore';
 
-const rawStore = createStore(x => x);
+const endo = _ => _;
+
+const initialState = {};
 
 /**
  * @type {RichStore}
  */
-export const sharedStore = new RichStore(rawStore);
+export const sharedStore = new RichStore(createStore(
+  endo,
+  initialState,
+  applyMiddleware(seal),
+));
