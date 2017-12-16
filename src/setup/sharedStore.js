89bae76c0936d3988b57423e100a3be2d12e7e54
x7c1 <jmtaro+x7c1@gmail.com>
2017-12-16 19:53:18 +0900

    remove unused initial state

--- src/setup/sharedStore.js
+++ src/setup/sharedStore.js
@@ -4,13 +4,10 @@ import { RichStore } from 'general/RichStore';
 
 const endo = _ => _;
 
-const initialState = {};
-
 /**
  * @type {RichStore}
  */
 export const sharedStore = new RichStore(createStore(
   endo,
-  initialState,
   applyMiddleware(capsule),
 ));
